40f6bd17cc59cf77c81ac82b2c4e9563a638f3ae
Brant Bagnall <brant.bagnall@gmail.com>
2017-10-27 12:07:12 -0600

    testing

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import React, { Component } from 'react';
 import './App.css';
 import {HashRouter, Route} from 'react-router-dom';
 import login from './components/Login/Login.js';
+import header from './components/Header/Header.js';//for testing
+
 
 
 class App extends Component {
   render() {
     return (
       <HashRouter>
-        <Route path='/' component={login} exact />
+        <div>
+          <Route path='/' component={login} exact />
+          <Route path='/header' component={header} />
+        </div>
       </HashRouter>
     );
   }
