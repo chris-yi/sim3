3b0101afd1be2c3e7aba4a468329050cdecf09af
Brant Bagnall <brant.bagnall@gmail.com>
2017-10-27 15:02:26 -0600

    more dashboard work

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -8,21 +8,21 @@ export default class Dashboard extends Component {
             <div>
             <Header className='dashboard-header' />
                 <div className='dashboard-background' >
-                    <div>
+                    <div className='dashboard-main' >
                         <div className='dashboard-top' >
                             <div className='dashboard-profile' >
-                                <div><img src='https://robohash.org/me' alt='profile pic' /></div>
+                                <div><img src='https://robohash.org/me' alt='profile pic' className='dashboard-profile-pic' /></div>
                                 <div>
                                     <h1>firstname</h1>
                                     <h1>lastname</h1>
                                     <button>Edit Profile</button>
                                 </div>
                             </div>
-                            <div>
+                            <div className='dashboard-lorem' >
                                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sem turpis, malesuada a cursus vitae, lobortis et neque. Morbi a ex lectus. Aenean eu ornare orci. Morbi vestibulum tincidunt risus, eget rhoncus odio mattis a.</p>
                             </div>
                         </div>
-                        <div>
+                        <div className='dashboard-bottom' >
 
                         </div>
                     </div>
