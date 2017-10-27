c23ea18a0de69db3f1b424f8f75aefd5c3d589ba
Brant Bagnall <brant.bagnall@gmail.com>
2017-10-27 11:28:56 -0600

    added first route

---
+++ src/components/Login/Login.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+export default class Login extends Component {
+    render() {
+        return (
+            <div>
+                <div>
+                    <img />
+                    <h1>
+                        Helo
+                    </h1>
+                    <button>
+                        Login/Register
+                    </button>
+                </div>
+            </div>
+        )
+    }
+}
