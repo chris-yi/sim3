d4e067f5cc2bd7d40185ecd7431f927002b74a0e
DragonMasterDyne <turbo675@gmail.com>
2017-10-27 12:57:57 -0600

    sh

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,24 +1,29 @@
 import React, { Component } from 'react';
 import homeImg from '../assets/home.png';
 import searchImg from '../assets/search.png';
+import './header.css';
 
 export default class Header extends Component {
     render() {
     return (
         <div className='header-main' >
-            <div >
+            <div className='header-left' >
                 <h1>Helo</h1>
-                <button>
-                    <img src={homeImg} alt='home button' />
-                </button>
-                <button>
-                    <img src={searchImg} alt='search button' />
-                </button>
+                <div>
+                    <a>
+                        <img src={homeImg} alt='home button' />
+                    </a>
+                </div>
+                <div>
+                    <a>
+                        <img src={searchImg} alt='search button' />
+                    </a>
+                </div>
             </div>
-            <div>
+            <div className='header-links' >
                 <h1>Dashboard</h1>
             </div>
-            <div>
+            <div className='header-links' >
                 <h1>Logout</h1>
             </div>
         </div>
