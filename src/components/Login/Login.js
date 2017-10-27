06d3658142e3d7d84b6e3dd198d5e5d1a4029684
Brant Bagnall <brant.bagnall@gmail.com>
2017-10-27 13:01:07 -0600

    hope this works

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './Login.css';
+import './login.css';
 import logo from "./../assets/logo.png";
 
 
@@ -13,7 +13,7 @@ export default class Login extends Component {
                         <h1 className="helo">Helo</h1>
                     </div>
                     <div className="loginButton">
-                    <a href="" className="loginLink">Login / Register</a>
+                    <a href='http://localhost:3005/api/auth/login' className="loginLink">Login / Register</a>
                     </div>
                 </div>
             </div>
