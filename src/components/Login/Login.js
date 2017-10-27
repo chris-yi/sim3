f9ed7f319117424cd7372812848dcc42b9bd7c4e
Brant Bagnall <brant.bagnall@gmail.com>
2017-10-27 13:23:09 -0600

    added proxy

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -13,11 +13,7 @@ export default class Login extends Component {
                         <h1 className="helo">Helo</h1>
                     </div>
                     <div className="loginButton">
-<<<<<<< HEAD
-                    <a href='http://localhost:3005/api/auth/login' className="loginLink">Login / Register</a>
-=======
-                    <a href={process.env.APP_LOGIN} className="loginLink">Login / Register</a>
->>>>>>> d932b88f6c0a0be4cfddc9c124270b1d884668b7
+                        <a href={process.env.APP_LOGIN} className="loginLink" >Login/Register</a>
                     </div>
                 </div>
             </div>
