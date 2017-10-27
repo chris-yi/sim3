f9d06547548ebb6fcc775462e5a2cd4a4e0c879a
DragonMasterDyne <turbo675@gmail.com>
2017-10-27 11:18:59 -0600

    server

--- server/index.js
+++ server/index.js
@@ -1,1 +1,8 @@
-ljlkjj
+require('dotenv').config()
+const express = require('express'),
+      bodyParser = require('body-parser'),
+      massive = require('massive'),
+      session = require('express-session'),
+      cors = require('cors');
+      passport = require('passport'),
+      Auth0Strategy = require('passport-auth0')
